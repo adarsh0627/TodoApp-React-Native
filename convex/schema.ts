a0dfcc1848ec302c00c79201d251f3ef75ab5408
Adarsh Srivastava <adarshsrivastava868@gmail.com>
2025-10-05 23:05:04 +0530

    Initial commit - todo app

---
+++ convex/schema.ts
@@ -0,0 +1,10 @@
+import {defineTable, defineSchema} from "convex/server";
+
+import {v} from "convex/values";
+
+export default defineSchema({
+    todos: defineTable({
+        text: v.string(),
+        isCompleted: v.boolean(),
+    })
+})
